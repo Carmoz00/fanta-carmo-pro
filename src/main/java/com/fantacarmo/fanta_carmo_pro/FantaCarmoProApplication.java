5fb686f39a28b8f1ae6e5bbe7ddc533c36f510e1
Carmelo Panepinto <carmelo.panepinto@liparipeople.com>
2025-09-20 19:36:20 +0200

    initial commit

---
+++ src/main/java/com/fantacarmo/fanta_carmo_pro/FantaCarmoProApplication.java
@@ -0,0 +1,13 @@
+package com.fantacarmo.fanta_carmo_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaCarmoProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantaCarmoProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantacarmo/fanta_carmo_pro/FantaCarmoProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantacarmo.fanta_carmo_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaCarmoProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
